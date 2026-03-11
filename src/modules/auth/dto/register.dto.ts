c58dc515ec88053f34ed517cf36ebfcba41c2fe2
baransural <baransural3@gmail.com>
2026-03-12 00:09:37 +0300

    feat: update password validation to use strong password criteria

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword } from 'class-validator';
 
 export class RegisterDto {
   @IsString()
@@ -9,6 +9,18 @@ export class RegisterDto {
   email: string;
 
   @IsString()
-  @MinLength(6)
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'Şifre çok zayıf! (En az 8 karakter, 1 büyük harf, 1 küçük harf, 1 sayı ve 1 özel karakter içermeli)',
+    },
+  )
   password: string;
 }
